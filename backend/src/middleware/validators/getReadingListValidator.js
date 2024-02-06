10c3eed9bc0e472865ddbdbeea4c91ae3be0597c
atlantis-11 <85559317+atlantis-11@users.noreply.github.com>
2024-02-06 15:36:33 +0200

    Made status required when getting reading list

--- backend/src/middleware/validators/getReadingListValidator.js
+++ backend/src/middleware/validators/getReadingListValidator.js
@@ -2,12 +2,14 @@ const { query } = require('express-validator');
 const { BOOK_STATUSES } = require('../../config/constants');
 
 module.exports = [
-    query('status').optional().custom((value) => {
-        if (!Object.values(BOOK_STATUSES).includes(value)) {
-            throw new Error('Invalid status');
-        }
-        return true;
-    }),
+    query('status')
+        .custom((value) => {
+            if (!Object.values(BOOK_STATUSES).includes(value)) {
+                throw new Error('Invalid status');
+            }
+            return true;
+        })
+        .notEmpty().withMessage('Status is required'),
     query('after').optional().isISO8601().withMessage('Date has to be in ISO 8601 format').toDate(),
     query('before').optional().isISO8601().withMessage('Date has to be in ISO 8601 format').toDate()
 ];
